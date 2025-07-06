4b9b354a8836e6d54baf397ff98f1bb2a6eafba3
youcef <youcefdz0dz@gmail.com>
2025-07-06 14:16:54 +0100

    Added the Trending section

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './component/Nav'
+import Trends from './component/Trends'
 
 const App = ()=> {
   return (
-    <>
+    <div className="bg-black  text-white">
       <Nav />
-    </>
+      <Trends />
+    </div>
   )
 }
 
---
+++ netflix-clone/src/component/Trends.jsx
@@ -0,0 +1,60 @@
+import Nav from './Nav'
+import img01 from '../assets/img01.webp'
+import img02 from '../assets/img02.webp'
+function Trends() {
+
+    const movies = [
+        {
+            "id": 1,
+            "url":img01
+        },
+        {
+            "id": 2,
+            "url":img02
+        },
+        {
+            "id": 3,
+            "url":img01
+        },
+        {
+            "id": 4,
+            "url":img02
+        },
+        {
+            "id": 5,
+            "url":img01
+        },
+        {
+            "id": 6,
+            "url":img02
+        },
+        {
+            "id": 7,
+            "url":img01
+        },
+        {
+            "id": 8,
+            "url":img02
+        },
+    ]
+  return (
+    <div className='pt-10 p-10'>
+        <div className='font-bold text-2xl '>Trending Now</div>
+            <div className='flex gap-10   overflow-scroll hide-scrollbar'>
+        {movies.map((movie , index) => {
+            return(
+                  <div key={index} className='pt-6 relative '>
+                     <img className="rounded-2xl min-w-[150px]" src={movie.url} alt="" />
+                         <div className='text-9xl font-bold absolute bottom-0 left-[-25px] text-stroke-white'>
+                                {movie.id}
+                        </div>   
+                </div>
+            )
+        })}
+            
+            </div>
+    </div>
+  )
+}
+
+export default Trends
